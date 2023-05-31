03cc843968c6ab4a2c956bf3774bcdf465521cd3
SantiB02 <santibrasca02@gmail.com>
2023-05-31 12:53:12 -0300

    Molde

---
+++ TP_Integrador/Integrador_GRUPO1.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+class ProgramaPrincipal():
+    def menu():
+        repite = 1
+        while repite != 0:
+            print("LIBRERÍA 'Buscalibre' - Menú de opciones")
+            print("1- Cargar Libros") #Coria
+            print("2- Modificar precio de un libro") #Santi
+            print("3- Borrar un libro")
+            print("4- Cargar disponibilidad") #Ivo
+            print("5- Listado de libros")
+            print("6- Ventas")
+            print("7- Actualizar Precios")
+            print("8- Registros anteriores a una fecha")
+            print("0- Salir del menú")
+            respuesta = int(input("Ingrese una opción del menú:"))
+        if respuesta === 1:
+            #Coria
+        if respuesta === 2:
+
+        if respuesta === 3:
+
+        if respuesta === 4:
+
+        if respuesta === 5:
+
+        if respuesta === 6:
+
+        if respuesta === 7:
+
+        if respuesta === 8:
+
+        if respuesta === 0:
+            repite = 0
+
+#class Conexiones():
+
+#class Libro():
+
+
+programa = ProgramaPrincipal()
