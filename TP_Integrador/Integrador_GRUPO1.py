7cfea2d4eeb38ce0de1d0159bf70ca262d3d12b6
CoriaEzequiel <zequielcoria@gmail.com>
2023-06-03 03:55:33 -0300

    first commit
    carga de datos de un libro

--- TP_Integrador/Integrador_GRUPO1.py
+++ TP_Integrador/Integrador_GRUPO1.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 class ProgramaPrincipal():
-    def menu():
+    def menu(self):
         repite = 1
         while repite != 0:
             print("LIBRERÍA 'Buscalibre' - Menú de opciones")
@@ -15,28 +15,45 @@ class ProgramaPrincipal():
             print("8- Registros anteriores a una fecha")
             print("0- Salir del menú")
             respuesta = int(input("Ingrese una opción del menú:"))
-        if respuesta === 1:
-            #Coria
-        if respuesta === 2:
+            if respuesta == 1:
+                self.cargar_libros()  #Coria
+            if respuesta == 2:
+                self.modificar_precio_libro()
+            if respuesta == 3:
+                self.borrar_libro()
+            if respuesta == 4:
+                self.cargar_disponibilidad()
+            if respuesta == 5:
+                self.listado_libros()
+            if respuesta == 6:
+                self.ventas()
+            if respuesta == 7:
+                self.actualizar_precios()
+            if respuesta == 8:
+                self.registros_anteriores()
+            if respuesta == 0:
+                repite = 0
 
-        if respuesta === 3:
+    def cargar_libros(self):
+        # Conectarse a la base de datos
+        conexion = sqlite3.connect('libros.db')
+        cursor = conexion.cursor()
 
-        if respuesta === 4:
+        # Solicitar los datos al usuario para un libro
+        id_libro = int(input("Ingrese el ID del libro: "))
+        isbn = input("Ingrese el ISBN del libro: ")
+        titulo = input("Ingrese el título del libro: ")
+        autor = input("Ingrese el autor del libro: ")
+        genero = input("Ingrese el género del libro: ")
+        precio = float(input("Ingrese el precio del libro: "))
 
-        if respuesta === 5:
+        # Insertar los datos en la base de datos
+        cursor.execute("INSERT INTO libros (ID, ISBN, Título, Autor, Género, Precio) VALUES (?, ?, ?, ?, ?, ?)",
+                       (id_libro, isbn, titulo, autor, genero, precio))
 
-        if respuesta === 6:
-
-        if respuesta === 7:
-
-        if respuesta === 8:
-
-        if respuesta === 0:
-            repite = 0
-
-#class Conexiones():
-
-#class Libro():
-
+        # Guardar los cambios y cerrar la conexión
+        conexion.commit()
+        conexion.close()
 
 programa = ProgramaPrincipal()
+programa.menu()
