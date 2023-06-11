31156a58c473f107e410378eb77d17a2d714b260
SantiB02 <santibrasca02@gmail.com>
2023-06-11 18:48:12 -0300

    completada clase Conexiones e identacion

--- TP_Integrador/Integrador_GRUPO1.py
+++ TP_Integrador/Integrador_GRUPO1.py
@@ -1,14 +1,15 @@
 import sqlite3
 
+
 class ProgramaPrincipal():
     def menu(self):
         repite = 1
         while repite != 0:
             print("LIBRERÍA 'Buscalibre' - Menú de opciones")
-            print("1- Cargar Libros") #Coria
-            print("2- Modificar precio de un libro") #Santi
+            print("1- Cargar Libros")  # Coria
+            print("2- Modificar precio de un libro")  # Santi
             print("3- Borrar un libro")
-            print("4- Cargar disponibilidad") #Ivo
+            print("4- Cargar disponibilidad")  # Ivo
             print("5- Listado de libros")
             print("6- Ventas")
             print("7- Actualizar Precios")
@@ -16,7 +17,7 @@ class ProgramaPrincipal():
             print("0- Salir del menú")
             respuesta = int(input("Ingrese una opción del menú:"))
             if respuesta == 1:
-                self.cargar_libros()  #Coria
+                self.cargar_libros()  # Coria
             if respuesta == 2:
                 self.modificar_precio_libro()
             if respuesta == 3:
@@ -34,82 +35,100 @@ class ProgramaPrincipal():
             if respuesta == 0:
                 repite = 0
 
-#Falta un método de ProgramaPrincipal para crear tablas a usar
-def crear_tablas(self):
-    conexion = Conexiones()
-    conexion.abrir_conexion()
-    try:
-        conexion.mi_cursor.execute("DROP TABLE IF EXISTS Libros")
-        conexion.mi_cursor.execute("""
-            CREATE TABLE Libros (
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                ISBN TEXT UNIQUE,
-                Titulo TEXT,
-                Autor TEXT,
-                Genero TEXT,
-                Precio REAL,
-                FechaUltimoPrecio TEXT,
-                CantDisponible INTEGER
-            )
-        """)
-        conexion.mi_cursor.execute("DROP TABLE IF EXISTS Ventas")
-        conexion.mi_cursor.execute("""
-            CREATE TABLE Ventas (
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                LibroID INTEGER,
-                Cantidad INTEGER,
-                FechaVenta TEXT,
-                FOREIGN KEY (LibroID) REFERENCES Libros(ID)
-            )
-        """)
-        conexion.mi_cursor.execute("DROP TABLE IF EXISTS historico_libros")
-        conexion.mi_cursor.execute("""
-            CREATE TABLE historico_libros (
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                ISBN TEXT UNIQUE,
-                Titulo TEXT,
-                Autor TEXT,
-                Genero TEXT,
-                Precio REAL,
-                FechaUltimoPrecio TEXT,
-                CantDisponible INTEGER
-            )
-        """)
-        conexion.mi_conexion.commit()
-        print("Tablas creadas exitosamente.")
-    except:
-        print("Error al crear las tablas.")
-    finally:
-        conexion.cerrar_conexion()
-
-    
-def cargar_libros(self):
-    try:
-        isbn = input("Ingrese el ISBN del libro: ")
-        titulo = input("Ingrese el título del libro: ")
-        autor = input("Ingrese el autor del libro: ")
-        genero = input("Ingrese el género del libro: ")
-        precio = float(input("Ingrese el precio del libro: "))
-        fecha_ultimo_precio = input("Ingrese la fecha del último precio (DD-MM-YYYY): ")
-        cant_disponible = int(input("Ingrese la cantidad disponible del libro: "))
-
+    def crear_tablas(self): #método de ProgramaPrincipal para crear tablas a usar
         conexion = Conexiones()
         conexion.abrir_conexion()
-        conexion.mi_cursor.execute("""
-            INSERT INTO Libros (ISBN, Titulo, Autor, Genero, Precio, FechaUltimoPrecio, CantDisponible)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (isbn, titulo, autor, genero, precio, fecha_ultimo_precio, cant_disponible))
-        conexion.mi_conexion.commit()
-        print("Libro cargado exitosamente.")
-    except:
-        print("Error al cargar el libro.")
-    finally:
-        conexion.cerrar_conexion()
+        try:
+            conexion.mi_cursor.execute("DROP TABLE IF EXISTS Libros")
+            conexion.mi_cursor.execute("""
+                CREATE TABLE Libros (
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    ISBN TEXT UNIQUE,
+                    Titulo TEXT,
+                    Autor TEXT,
+                    Genero TEXT,
+                    Precio REAL,
+                    FechaUltimoPrecio TEXT,
+                    CantDisponible INTEGER
+                )
+            """)
+            conexion.mi_cursor.execute("DROP TABLE IF EXISTS Ventas")
+            conexion.mi_cursor.execute("""
+                CREATE TABLE Ventas (
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    LibroID INTEGER,
+                    Cantidad INTEGER,
+                    FechaVenta TEXT,
+                    FOREIGN KEY (LibroID) REFERENCES Libros(ID)
+                )
+            """)
+            conexion.mi_cursor.execute("DROP TABLE IF EXISTS historico_libros")
+            conexion.mi_cursor.execute("""
+                CREATE TABLE historico_libros (
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    ISBN TEXT UNIQUE,
+                    Titulo TEXT,
+                    Autor TEXT,
+                    Genero TEXT,
+                    Precio REAL,
+                    FechaUltimoPrecio TEXT,
+                    CantDisponible INTEGER
+                )
+            """)
+            conexion.mi_conexion.commit()
+            print("Tablas creadas exitosamente.")
+        except:
+            print("Error al crear las tablas.")
+        finally:
+            conexion.cerrar_conexion()
+
+
+    def cargar_libros(self):
+        try:
+            isbn = input("Ingrese el ISBN del libro: ")
+            titulo = input("Ingrese el título del libro: ")
+            autor = input("Ingrese el autor del libro: ")
+            genero = input("Ingrese el género del libro: ")
+            precio = float(input("Ingrese el precio del libro: "))
+            fecha_ultimo_precio = input(
+                "Ingrese la fecha del último precio (DD-MM-YYYY): ")
+            cant_disponible = int(
+                input("Ingrese la cantidad disponible del libro: "))
+
+            conexion = Conexiones()
+            conexion.abrir_conexion()
+            conexion.mi_cursor.execute("""
+                INSERT INTO Libros (ISBN, Titulo, Autor, Genero, Precio, FechaUltimoPrecio, CantDisponible)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (isbn, titulo, autor, genero, precio, fecha_ultimo_precio, cant_disponible))
+            conexion.mi_conexion.commit()
+            print("Libro cargado exitosamente.")
+        except:
+            print("Error al cargar el libro.")
+        finally:
+            conexion.cerrar_conexion()
+
 
 class Conexiones():
-    def abrirConexion(self):
-        self.conexion = sqlite3.connect('libros.db')
-        self.cursor = self.conexion.cursor()
+    def __init__(self):
+        self.mi_conexion = None
+        self.mi_cursor = None
+
+    def abrir_conexion(self):
+        try:
+            self.mi_conexion = sqlite3.connect("libreria.db")
+            self.mi_cursor = self.mi_conexion.cursor()
+        except:
+            print("Error al abrir la conexión con la base de datos.")
+
+    def cerrar_conexion(self):
+        try:
+            self.mi_cursor.close()
+            self.mi_conexion.close()
+        except:
+            print("Error al cerrar la conexión con la base de datos.")
+
 
 programa = ProgramaPrincipal()
+programa.crear_tablas()
 programa.menu()
